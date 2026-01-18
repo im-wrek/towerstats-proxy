562c2643cb1a2b361346bef4dc8f276b650b353d
wrek <87952649+im-wrek@users.noreply.github.com>
2026-01-18 14:35:43 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: true, // enables /app folder usage
-  },
-  compiler: {
-    styledComponents: false, // no extra UI libraries
-  },
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-};
+  reactStrictMode: true
+}
 
-export default nextConfig;
+module.exports = nextConfig;
