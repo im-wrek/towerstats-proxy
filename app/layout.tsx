4b0485b1e3757e84e5b5d44b13ad55ba75a1b056
wrek <87952649+im-wrek@users.noreply.github.com>
2026-01-19 15:08:40 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
+"use client";
+
 import { Analytics } from "@vercel/analytics/react";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <head>
         <title>TowerStats Proxy</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body>
         {children}
